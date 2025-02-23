2e4d556a4e214451c09da4b851186cef422a163e
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-02-23 13:14:29 +0300

    CheckStyle fix.

--- src/exceptions/ManagerLoadException.java
+++ src/exceptions/ManagerLoadException.java
@@ -4,6 +4,7 @@ public class ManagerLoadException extends RuntimeException {
     public ManagerLoadException(String message) {
         super(message);
     }
+
     public ManagerLoadException(Exception ex) {
         super(ex);
     }
