25c17aba8203aa6e13070734d093548fd817399b
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-02-23 13:12:48 +0300

    CheckStyle fix.

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -4,7 +4,7 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message) {
         super(message);
     }
-    
+
     public ManagerSaveException(Exception ex) {
         super(ex);
     }
