04bc022c8c1fd0064ae467acca231e875d7871da
DvoychenkovA <DvoychenkovA@terralink-global.com>
2025-02-28 12:52:50 +0300

    CheckStyle fix.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import utilities.Managers;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
